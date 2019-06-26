0ce5acac11a57306e4550630b124bb98315d3476
Bastiani <rafacdb@gmail.com>
2019-06-26 15:47:49 -0300

    subscription

--- app/Resolvers/Product.js
+++ app/Resolvers/Product.js
@@ -1,15 +1,35 @@
 const Product = use("App/Models/Product");
+const { PubSub } = require("apollo-server");
+
+const pubsub = new PubSub();
+
+const EVENTS = {
+  PRODUCT: {
+    ADDED: "PRODUCT_ADDED"
+  }
+};
 
 module.exports = {
   Query: {
-    products: async () => {
+    products: async (parent, args, { foo }, info) => {
+      // console.log("======== context", parent, foo, args, info);
+
       const products = await Product.all();
       return products.toJSON();
     }
   },
   Mutation: {
     addProduct: async (_, { description, price }) => {
-      return await Product.create({ id: 1, description, price });
+      const product = await Product.create({ id: 1, description, price });
+      pubsub.publish(EVENTS.PRODUCT.ADDED, { product });
+      return product;
+    }
+  },
+  Subscription: {
+    productAdded: {
+      // Additional event labels can be passed to asyncIterator creation
+      resolve: ({ product }) => product,
+      subscribe: () => pubsub.asyncIterator([EVENTS.PRODUCT.ADDED])
     }
   }
 };
--- providers/Apollo/ApolloProvider.js
+++ providers/Apollo/ApolloProvider.js
@@ -1,5 +1,5 @@
 const { ServiceProvider } = require("@adonisjs/fold");
-const { ApolloServer, gql } = require("adonis-apollo2");
+const { ApolloServer } = require("adonis-apollo2");
 const { makeExecutableSchema } = require("graphql-tools");
 const {
   fileLoader,
@@ -15,6 +15,7 @@ class ApolloProvider extends ServiceProvider {
   boot() {
     const Route = use("Route");
     const config = use("Config");
+    const Server = use("Server");
 
     const typeDefs = mergeTypes(
       fileLoader(config.get("graphql.schema"), { recursive: true })
@@ -22,13 +23,29 @@ class ApolloProvider extends ServiceProvider {
     const resolvers = mergeResolvers(
       fileLoader(config.get("graphql.resolvers"))
     );
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-    const options = config.get("graphql.options");
+    // const schema = makeExecutableSchema({ typeDefs, resolvers });
+    // const options = config.get("graphql.options");
     const server = new ApolloServer({
       typeDefs,
-      resolvers
+      resolvers,
+      context: async ({ req, connection }) => {
+        if (!req || !req.headers) {
+          return;
+        }
+
+        return { authorized: true };
+      },
+      tracing: true
     });
-    server.registerRoutes({ router: Route });
+    // server.installSubscriptionHandlers(Server);
+    server.registerRoutes({
+      router: Route,
+      disableHealthCheck: false,
+      onHealthCheck: ctx => {
+        console.log("======= ctx", ctx);
+        return Promise.reject();
+      }
+    });
     // this.app.singleton("ApolloProvider", () => {
     //   console.log("Apollo from my provider");
     //   const server = new ApolloServer({
