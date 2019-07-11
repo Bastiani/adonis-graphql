aef7a677f543a98b325518751310fedcbedb279e
Bastiani <rafacdb@gmail.com>
2019-07-11 15:32:31 -0300

    feat(pagination): send cursor pagination to file

---
+++ app/GraphQL/Cursor.js
@@ -0,0 +1,47 @@
+function cursor(data, after, first) {
+  if (first < 0) {
+    throw new Error("First must be positive");
+  }
+
+  const totalCount = data.length;
+  let dataReturned = [];
+  let start = 0;
+  if (after !== undefined) {
+    const buff = new Buffer(after, "base64");
+    const id = buff.toString("ascii");
+    const index = data.findIndex(obj => obj.id === id);
+    if (index === -1) {
+      throw new Error("After does not exist");
+    }
+    start = index + 1;
+  }
+  dataReturned = first === undefined ? data : data.slice(start, start + first);
+
+  let endCursor;
+  const edges = dataReturned.map(user => {
+    const buffer = new Buffer(user._id);
+    endCursor = buffer.toString("base64");
+    return {
+      cursor: endCursor,
+      node: user
+    };
+  });
+  const hasNextPage = start + first < totalCount;
+  const pageInfo =
+    endCursor !== undefined
+      ? {
+          endCursor,
+          hasNextPage
+        }
+      : {
+          hasNextPage
+        };
+  const result = {
+    edges,
+    pageInfo,
+    totalCount
+  };
+  return result;
+}
+
+module.exports = cursor;
--- app/Resolvers/User.js
+++ app/Resolvers/User.js
@@ -1,5 +1,6 @@
 const User = use("App/Models/User");
 const { PubSub } = require("apollo-server");
+const cursor = use("App/GraphQL/Cursor");
 
 const pubsub = new PubSub();
 
@@ -12,53 +13,9 @@ const EVENTS = {
 module.exports = {
   Query: {
     users: async (parent, { after, first }) => {
-      if (first < 0) {
-        throw new Error("First must be positive");
-      }
-
       const data = await User.all();
       const dataJSON = data.toJSON();
-
-      const totalCount = dataJSON.length;
-      let users = [];
-      let start = 0;
-      if (after !== undefined) {
-        const buff = new Buffer(after, "base64");
-        const id = buff.toString("ascii");
-        const index = dataJSON.findIndex(user => user.id === id);
-        if (index === -1) {
-          throw new Error("After does not exist");
-        }
-        start = index + 1;
-      }
-      users =
-        first === undefined ? dataJSON : dataJSON.slice(start, start + first);
-
-      let endCursor;
-      const edges = users.map(user => {
-        const buffer = new Buffer(user._id);
-        endCursor = buffer.toString("base64");
-        return {
-          cursor: endCursor,
-          node: user
-        };
-      });
-      const hasNextPage = start + first < totalCount;
-      const pageInfo =
-        endCursor !== undefined
-          ? {
-              endCursor,
-              hasNextPage
-            }
-          : {
-              hasNextPage
-            };
-      const result = {
-        edges,
-        pageInfo,
-        totalCount
-      };
-      return result;
+      return cursor(dataJSON, after, first);
     }
   },
   Mutation: {
